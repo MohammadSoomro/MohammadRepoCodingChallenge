8e7dbaf8155b00a521310a10da3bd61db345b1dc
ubaidullah.niaz <ubaidullah.niaz@IC-CS233-29.uc.ucalgary.ca>
2022-07-22 11:50:50 -0600

    Added my branch label to partners repo

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Ubaid's label"));
+			root.getChildren().add(new Label("Ubaid's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Mohammad's project");
